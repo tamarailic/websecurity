a243a843143db11de4b215089f170466fb9827ad
Tamara Ilic <tamarailic11@gmail.com>
2023-03-24 23:20:32 +0100

    BREAKING_CHANGE:Project init

---
+++ src/test/java/com/websecurity/websecurity/WebsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websecurity.websecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
