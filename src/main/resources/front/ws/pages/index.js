8b9601941dc243a078793f44368df8d94cccce72
Uros Pocek <uros.pocek@gmail.com>
2023-04-07 22:54:09 +0200

    feat: Added filter section for main card

--- src/main/resources/front/ws/pages/index.js
+++ src/main/resources/front/ws/pages/index.js
@@ -9,40 +9,81 @@ export default function Home() {
 }
 
 function HomePage() {
-  const [optionSelected, setOptionSelected] = useState(0);
-  const certificates = getAllCertificates();
+  const [selectedSection, setSelectedSection] = useState(0);
+  const [selectedItem, setSelectedItem] = useState(null);
+  const [appliedFilters, setAppliedFilters] = useState({ search: null, type: null, my: false });
 
-  function toogle_option(i) {
-    setOptionSelected(i);
+  function showSection(i) {
+    setSelectedSection(i);
   }
 
-  return <>
+  return (
     <section className={styles.main_grid}>
-      <div className={styles.card} id="all_elements_table">
-        <div className={styles.toggle_options}>
-          <p onClick={() => toogle_option(0)} className={optionSelected == 0 ? styles.selected : ''}>All certificates</p>
-          <div className={styles.separator}></div>
-          <p onClick={() => toogle_option(1)} className={optionSelected == 1 ? styles.selected : ''}>My requests</p>
-          <div className={styles.separator}></div>
-          <p onClick={() => toogle_option(2)} className={optionSelected == 2 ? styles.selected : ''}>For signing</p>
-        </div>
-        <div className={styles.filter_options}></div>
-        <div id="main_area">
-          <table className={styles.main_table}>
-            <thead>
-              <tr>
-
-              </tr>
-            </thead>
-
-
-          </table>
-        </div>
-      </div>
-      <div className={styles.card} id="one_element_preview"></div>
-    </section >
-  </>
+      <AllElementsTable selectedSection={selectedSection} showSection={showSection} setSelectedItem={setSelectedItem} appliedFilters={appliedFilters} setAppliedFilters={setAppliedFilters} />
+      <OneElementPreview selectedItem={selectedItem} />
+    </section >);
 }
+
+function AllElementsTable({ selectedSection, showSection, setSelectedItem, appliedFilters, setAppliedFilters }) {
+  return (
+    <div className={styles.card} id="all_elements_table">
+      <ToggleOptions selectedSection={selectedSection} showSection={showSection} />
+      <FilterOptions appliedFilters={appliedFilters} setAppliedFilters={setAppliedFilters} />
+      <MainArea setSelectedItem={setSelectedItem} appliedFilters={appliedFilters} />
+    </div>
+  );
+}
+
+function ToggleOptions({ selectedSection, showSection }) {
+  return (
+    <div className={styles.toggle_options}>
+      <p onClick={() => showSection(0)} className={selectedSection == 0 ? styles.selected : ''}>All certificates</p>
+      <div className={styles.separator}></div>
+      <p onClick={() => showSection(1)} className={selectedSection == 1 ? styles.selected : ''}>My requests</p>
+      <div className={styles.separator}></div>
+      <p onClick={() => showSection(2)} className={selectedSection == 2 ? styles.selected : ''}>For signing</p>
+    </div>
+  );
+}
+
+function FilterOptions({ appliedFilters, setAppliedFilters }) {
+
+  function handleChange(event) {
+    appliedFilters['search'] = event.target.value;
+    setAppliedFilters(appliedFilters);
+  }
+
+
+  return (
+    <div className={styles.filter_options}>
+      <input id="" type="text" placeholder="Search..." onChange={handleChange} />
+
+    </div>
+  );
+}
+
+function MainArea({ setSelectedItem, appliedFilters }) {
+  const certificates = getAllCertificates();
+  return (
+    <div id="main_area">
+      <table className={styles.main_table}>
+        <thead>
+          <tr>
+
+          </tr>
+        </thead>
+
+
+      </table>
+    </div>
+  );
+}
+
+function OneElementPreview() {
+  return (<div className={styles.card} id="one_element_preview"></div>);
+}
+
+
 
 async function getAllCertificates() {
   return [{ t: 1, q: 2 }]
