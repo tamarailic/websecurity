40981c6938fa2d13f1db67cb580a2084f0d85372
Uros Pocek <uros.pocek@gmail.com>
2023-04-07 17:03:15 +0200

    feat: Added main page structure

--- src/main/resources/front/ws/pages/index.js
+++ src/main/resources/front/ws/pages/index.js
@@ -1,5 +1,5 @@
 import PageContainer from "@/components/pageContainer"
-
+import styles from "@/styles/Home.module.css"
 
 export default function Home() {
   return (<PageContainer>
@@ -8,7 +8,31 @@ export default function Home() {
 }
 
 function HomePage() {
+
+  const certificates = getAllCertificates();
+
   return <>
-    <h1>Test2</h1>
+    <section className={styles.main_grid}>
+      <div id="all_elements_table">
+        <div className="toggle_options"></div>
+        <div className="filter_options"></div>
+        <div id="main_area">
+          <table className="main_table">
+            <thead>
+              <tr>
+
+              </tr>
+            </thead>
+
+
+          </table>
+        </div>
+      </div>
+      <div id="one_element_preview"></div>
+    </section >
   </>
+}
+
+async function getAllCertificates() {
+  return [{ t: 1, q: 2 }]
 }
