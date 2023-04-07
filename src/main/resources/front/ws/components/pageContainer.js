f040198a1c67f6dedc06429416e8f1e29d684c04
Uros Pocek <uros.pocek@gmail.com>
2023-04-07 22:14:45 +0200

    feat: Added table preview

--- src/main/resources/front/ws/components/pageContainer.js
+++ src/main/resources/front/ws/components/pageContainer.js
@@ -1,8 +1,14 @@
+import Head from "next/head";
 import NavBar from "./navBar";
 
 export default function PageContainer({ children }) {
     return (
         <>
+            <Head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+            </Head>
             <NavBar />
             {children}
         </>
--- src/main/resources/front/ws/pages/index.js
+++ src/main/resources/front/ws/pages/index.js
@@ -1,5 +1,6 @@
 import PageContainer from "@/components/pageContainer"
 import styles from "@/styles/Home.module.css"
+import { useState } from "react";
 
 export default function Home() {
   return (<PageContainer>
@@ -8,16 +9,26 @@ export default function Home() {
 }
 
 function HomePage() {
-
+  const [optionSelected, setOptionSelected] = useState(0);
   const certificates = getAllCertificates();
+
+  function toogle_option(i) {
+    setOptionSelected(i);
+  }
 
   return <>
     <section className={styles.main_grid}>
-      <div id="all_elements_table">
-        <div className="toggle_options"></div>
-        <div className="filter_options"></div>
+      <div className={styles.card} id="all_elements_table">
+        <div className={styles.toggle_options}>
+          <p onClick={() => toogle_option(0)} className={optionSelected == 0 ? styles.selected : ''}>All certificates</p>
+          <div className={styles.separator}></div>
+          <p onClick={() => toogle_option(1)} className={optionSelected == 1 ? styles.selected : ''}>My requests</p>
+          <div className={styles.separator}></div>
+          <p onClick={() => toogle_option(2)} className={optionSelected == 2 ? styles.selected : ''}>For signing</p>
+        </div>
+        <div className={styles.filter_options}></div>
         <div id="main_area">
-          <table className="main_table">
+          <table className={styles.main_table}>
             <thead>
               <tr>
 
@@ -28,7 +39,7 @@ function HomePage() {
           </table>
         </div>
       </div>
-      <div id="one_element_preview"></div>
+      <div className={styles.card} id="one_element_preview"></div>
     </section >
   </>
 }
