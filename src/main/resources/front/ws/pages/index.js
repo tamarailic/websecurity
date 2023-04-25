34db7973d4d54866b5e9f6ad1a72b83b743cecef
Tamara Ilic <tamarailic11@gmail.com>
2023-04-25 20:20:11 +0200

    fix:Fixing bugs

--- src/main/resources/front/ws/pages/index.js
+++ src/main/resources/front/ws/pages/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import useSWR from 'swr'
 import PageContainer from "@/components/pageContainer"
 import styles from "@/styles/Home.module.css"
@@ -11,8 +11,8 @@ import Error from "@/components/error";
 const fetcher = (...args) => fetch(...args).then(res => res.json());
 
 // Just mocked data -> should be replaced with data from JWT
-const userId = '6447f68495bfc35b4f3eb745';
-const username = 'tamarailic11@gmail.com';
+const userId = '643d966f681de87d29254e97';
+const username = 'aisling.rebyl@meantodeal.com';
 
 export default function Home() {
   return (<PageContainer>
@@ -341,6 +341,7 @@ async function approveCertificateRequest(requestId) {
 
 function DenyBtn({ requestId }) {
   const [denyReason, setDenyReason] = useState(null)
+  const ref = useRef(null)
 
   function handleDenyReason(event) {
     setDenyReason(event.target.value)
@@ -352,13 +353,14 @@ function DenyBtn({ requestId }) {
     } else {
       denyCertificateRequest(requestId, denyReason)
       setDenyReason(null)
+      ref.current.value = '';
     }
   }
 
   return <div className={styles.reasonDiv }>
     <div className={styles.reasonInput}>
       <label htmlFor="denyResaon">Denial reason:</label>
-      <input id="denyResaon" name="denyResaon" onChange={handleDenyReason} />
+      <input id="denyResaon" name="denyResaon" ref={ref} onChange={handleDenyReason} />
     </div>
     <div className={styles.denyBtn} onClick={handleDenial}>
       <a>Deny</a>
@@ -431,6 +433,7 @@ async function requestNewCertificate(userId, certificateType, issuerCertificateI
 
 function InvalidateButton({serialNumber}) {
   const [invalidationReason, setInvalidationReason] = useState(null)
+  const ref = useRef(null)
 
   function handleInvalidationReason(event) {
     setInvalidationReason(event.target.value)
@@ -442,12 +445,13 @@ function InvalidateButton({serialNumber}) {
     } else {
       invalidateCertificateRequest(serialNumber, invalidationReason)
       setInvalidationReason(null)
+      ref.current.value = '';
     }
   }
   return <div className={styles.reasonDiv }>
   <div className={styles.reasonInput}>
       <label htmlFor="invalidateReason">Invalidation reason:</label>
-      <input id="invalidateReason" name="invalidateReason"  onChange={handleInvalidationReason}/>
+      <input id="invalidateReason" name="invalidateReason" ref={ref} onChange={handleInvalidationReason}/>
     </div>
   <div className={styles.accentBtn} onClick={handleInvalidation}>
     <a>Invalidate</a>
@@ -469,7 +473,7 @@ async function invalidateCertificateRequest(serialNumber, reason){
   if (response.status == 200) {
     alert("Succesfully withdrawn certificate.")
   } else {
-    alert("Something went wrong!")
+    alert("You can invalidate already invalidated certificate!")
   }
 }
 
