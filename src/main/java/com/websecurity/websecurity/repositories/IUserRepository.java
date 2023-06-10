1ca73a4aff53fa7c1e95b77d233c726d209ef23e
Tamara Ilic <tamarailic11@gmail.com>
2023-06-10 19:50:51 +0200

    feat: download private key

--- src/main/java/com/websecurity/websecurity/repositories/IUserRepository.java
+++ src/main/java/com/websecurity/websecurity/repositories/IUserRepository.java
@@ -3,7 +3,9 @@ package com.websecurity.websecurity.repositories;
 import com.websecurity.websecurity.models.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface IUserRepository extends MongoRepository<User, String> {
     User findByUsername(String username);
-
+    Optional<User> findUserByUsername(String username);
 }
